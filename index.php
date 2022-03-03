b820d225e332a97a425b4c0b06fde024d56deafe
AlfredJohn98 <80449189+AlfredJohn98@users.noreply.github.com>
2022-03-04 01:31:54 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,457 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Alfred John</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+
+  <link href="assets/img/profile/favicon.png" rel="icon">
+  <link href="assets/img/profile/favicon.png" rel="apple-touch-icon">
+
+
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+  <header id="header">
+    <div class="container">
+
+      <h1><a href="index.html">Alfred John</a></h1>
+
+      <h2>Software Engineer | <span> Fresh Graduate</span> | Kuala Lumpur</h2>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Home</a></li>
+          <li><a class="nav-link" href="#about">About</a></li>
+          <li><a class="nav-link" href="#resume">Resume</a></li>
+          <li><a class="nav-link" href="#services">My Projects</a></li>
+          <li><a class="nav-link" href="#portfolio">Client Projects</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+      <div class="social-links">
+        <a href="mailto:alfredjohnspotify@gmail.com" target="_blank" class="mail"><i class="bi bi-inboxes-fill"></i></a>
+        <a href="https://github.com/alfredjohn98" target="_blank" class="github"><i class="bi bi-github"></i></a>
+        <a href="http://linkedin.com/in/alfred-john-3115171a0" target="_blank" class="linkedin"><i
+            class="bi bi-linkedin"></i></a>
+        <a href="https://wa.me/60129324039" target="_blank" class="whatsapp"><i class="bi bi-whatsapp"></i> </i></a>
+      </div>
+
+      <br>
+      <br>
+      <ul class="list-inline list-social-icons mb-0">
+        <li class="list-inline-item">
+          <a href="resume/cv.html" target="blank">
+            <button type="button" class="btn btn-light">Downlaod Resume</button>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+  </header>
+
+
+  <section id="about" class="about">
+
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p>About Me</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/profile/profile_pic.png" class="img-fluid" alt="Profile Pic">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Software Engineer</h3>
+          <p class="fst-italic">
+            My degree in Computer Science from University of Wollongong Australia. An enthusiastic and driven learner, I
+            am looking to enhance my practical knowledge in the information technology and business intelligence sector.
+            Adept at motivating self and others. Passionate about cloud computing and next generation of technology.
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>26 January 1998</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+60 12 932 4039</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Kuala Lumpur Wilayah
+                    Persekutuan</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>24</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Computer Science</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>alfredjohnspotify@gmail.com</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p>
+            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut.
+            Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+            Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+          </p>
+        </div>
+      </div>
+
+      <!-- ======= Skills  ======= -->
+      <div class="skills container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-4">
+
+            <div class="progress">
+              <span class="skill">C Programming<i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">C++<i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">PHP<i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4">
+
+            <div class="progress">
+              <span class="skill">Git <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Github <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Composer <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4">
+
+            <div class="progress">
+              <span class="skill">Bootstrap<i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Wordpress<i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Shopify Development<i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div><!-- End Skills -->
+
+    </div>
+
+  </section>
+
+  <section id="resume" class="resume">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Resume</h2>
+        <p>My Resume</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+
+          <h3 class="resume-title">Education</h3>
+          <div class="resume-item">
+            <h4>UNIVERSITY OF WOLLONGONG AUSTRALIA</h4>
+            <h5>March 2020 - February 2022</h5>
+            <p><em>BACHELORS OF COMPUTER SCIENCE</em></p>
+            <p>DOUBLE MAJOR : SOFTWARE ENGINNERING AND DIGITAL SYSTEM SECURITY</p>
+            <p>Percentage : Update</p>
+          </div>
+          <div class="resume-item">
+            <h4>INTI INTERNATIONAL COLLEGE SUBANG</h4>
+            <h5>August 2016 - January 2019</h5>
+            <p><em>DIPLOMA IN INFORMATION TECHNOLOGY AND COMMUNICATION</em></p>
+            <p>GPA: 3.00</p>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <h3 class="resume-title">Experience</h3>
+          <div class="resume-item">
+            <h4>IT Enginner</h4>
+            <h5>January 2019 - November 2019</h5>
+            <p <ul>
+              <li>Creating business website for client</li>
+              <br>
+              <li>Deployment and Migration of server</li>
+              <br>
+              <li> Managing firewalls - Cisco meraki nd Cyberoam</li>
+              <br>
+              <li>Deployment of Cicso layer three swicth for Vlan implemention</li>
+              <br>
+              <li>Creating business website for secondary company which was a startup</li>
+              <br>
+              <li>Deployment of a IT ticketing system for internal company usage</li>
+              <br>
+              <li>Manage and ensuring the availability and supportability of the technology of
+                &nbsp; &nbsp; &nbsp; client infrastructure</li>
+
+              </ul>
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+      <h3 class="resume-title">Certifications</h3>
+      <div class="resume-item pb-0">
+        <h4>Lean Six Sigma White Belt Certified - v4.0 </h4>
+        <p>
+        <ul>
+          <li>Management & Strategy Institute</li>
+          <li>Issued Aug 2020 </li>
+          <li>Credential ID : 57412573</li>
+        </ul>
+        </p>
+      </div>
+
+      <div class="resume-item pb-0">
+        <h4>Project Management Essentials Certified (PMEC) </h4>
+        <p>
+        <ul>
+          <li>Management & Strategy Institute</li>
+          <li>Issued Aug 2020 </li>
+          <li> Credential ID : 57431286</li>
+        </ul>
+        </p>
+      </div>
+
+      <div class="resume-item pb-0">
+        <h4>NSE 1 Network Security Associate</h4>
+        <p>
+        <ul>
+          <li>Fortinet</li>
+          <li>Issued Aug 2020 </li>
+          <li>Credential ID : nviRQ9j42p</li>
+        </ul>
+        </p>
+      </div>
+
+      <div class="resume-item pb-0">
+        <h4>NSE 2 Network Security Associate</h4>
+        <p>
+        <ul>
+          <li>Fortinet</li>
+          <li>Issued Aug 2020</li>
+          <li>Credential ID : 0Vis2lUiiq </li>
+        </ul>
+        </p>
+      </div>
+
+      <div class="resume-item pb-0">
+        <h4>Aws Cloud Practitioner Essentials</h4>
+
+      </div>
+
+  </section>
+
+  <section id="services" class="services">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Projects</h2>
+      </div>
+
+      <div class="col-lg-7">
+        <h3 class="resume-title">My Projects</h3>
+        <div class="resume-item">
+          <p <ul>
+
+            <li>Static Website for Individuals Client</li>
+            <br>
+            <li>Build Ecommerce Site with Payment Gateway</li>
+            <br>
+            <li>IT Helpdesk Software Deployment - SpiceWork</li>
+            <br>
+            <li>Insurance Claim System - University Project</li>
+            <br>
+            <li>Complete Library System with user roles - University Project</li>
+            <br>
+            <li> Employer Project : HR System for Malacca Securities Sdn Bhd (HQ) of Bursa Malaysia</li>
+            <br>
+
+            </ul>
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section id="portfolio" class="portfolio">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Client Projects</h2>
+        <p>Live Projects</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12 d-flex justify-content-center">
+          <ul id="portfolio-flters">
+            <li data-filter=".filter-web">Web Based Project</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row portfolio-container">
+
+        <div class="col-lg-6  col-md-6 portfolio-item filter-web">
+          <div class="portfolio-wrap">
+            <img src="assets/img/projects/project_1.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4></h4>
+              <p></p>
+              <div class="portfolio-links">
+                <a href="assets/img/projects/project_1.png" data-gallery="portfolioGallery"></a>
+                <a href="https://www.sparklink.biz/" target="_blank"><span>View</span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6  col-md-6 portfolio-item filter-web">
+          <div class="portfolio-wrap">
+            <img src="assets/img/projects/project_2.png" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4></h4>
+              <p></p>
+              <div class="portfolio-links">
+                <a href="assets/img/projects/project_2.png" data-gallery="portfolioGallery"></a>
+                <a href="https://farmandfish.com.my/" target="_blank">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <p>Connect With Me Via</p>
+      </div>
+
+      <div class="row mt-2">
+
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-envelope"></i>
+            <h3>Email Me</h3>
+            <p>alfredjohnspotify@gmail.com</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <div class="credits">
+    <a href="">Back To Top</a>
+  </div>
+
+
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
